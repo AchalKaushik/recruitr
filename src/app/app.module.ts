84f4c06a3b15061dac9aa6d0d61523e15db78a3b
Avi <axios47@gmail.com>
2017-08-13 14:29:48 +0530

    test commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ChallengeDetailComponent } from './challenge-detail/challenge-detail.component';
+import { ChallengeComponent } from './challenge/challenge.component';
+import { PnfComponent } from './pnf/pnf.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+
+const appRoutes: Routes = [
+  { path: 'welcome', component: HomeComponent },
+  // { path: 'hero/:id',      component: HeroDetailComponent },
+  // {
+  //   path: 'heroes',
+  //   component: HeroListComponent,
+  //   data: { title: 'Heroes List' }
+  // },
+  { path: '',
+    redirectTo: '/welcome',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PnfComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChallengeDetailComponent,
+    ChallengeComponent,
+    PnfComponent,
+    HomeComponent,
+    NavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
